378b993f4866a74eb9e9a3588ee9978f9676068e
agon <agon@hawaii.edu>
2014-02-26 12:08:14 -1000

    Setting project 2
    Maps library instead of geocharts

--- map.js
+++ map.js
@@ -1,27 +1,11 @@
-//Script to paint the USA map and World Map
+var map;
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644)
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
 
-//Load the Geochart Library
-google.load('visualization', '1', {packages: ['geochart']});
-
-//Function to paint the WorldMap
-function worldMap() {
-  var data = google.visualization.arrayToDataTable([
-    ['Country', 'Popularity', 'Cuantity'],
-    ['Spain', 700, 122],
-    ['United States', 300, 1223],
-    ['Brazil', 400, 900],
-    ['Canada', 500, 800],
-    ['France', 600, 722],
-    ['RU', 700, 300]
-  ]);
-  var World = new google.visualization.GeoChart(
-      document.getElementById('Worldmap'));
-  World.draw(data, {width: 710, height: 372});
-
-  //Function to get the name of the country we click on the world map
-  google.visualization.events.addListener(World, 'regionClick', function(eventData)
-    {
-      region = eventData.region;
-      console.log(region)
-    });
-};
+google.maps.event.addDomListener(window, 'load', initialize);
