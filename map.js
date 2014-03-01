2681fe71ea30297b0a0620c471f84453abbd0f38
agon <agon@hawaii.edu>
2014-02-28 14:44:43 -1000

    one language plotted on the map as a circle symbol and info when you click

--- map.js
+++ map.js
@@ -1,102 +1,62 @@
 var map;
 var TILE_SIZE = 256;
-var language = new google.maps.LatLng(41.850033,-87.6500523);
+var languageMap = {}; 
+
+languageMap['language1'] = {
+	center: new google.maps.LatLng(41.850033,-87.6500523)
+}; 
+
 var mapcenter = new google.maps.LatLng(43.5333,5.7000);
 
-function bound(value, opt_min, opt_max) {
-  if (opt_min != null) value = Math.max(value, opt_min);
-  if (opt_max != null) value = Math.min(value, opt_max);
-  return value;
-}
-
-function degreesToRadians(deg) {
-  return deg * (Math.PI / 180);
-}
-
-function radiansToDegrees(rad) {
-  return rad / (Math.PI / 180);
-}
-
-/** @constructor */
-function MercatorProjection() {
-  this.pixelOrigin_ = new google.maps.Point(TILE_SIZE / 2,
-      TILE_SIZE / 2);
-  this.pixelsPerLonDegree_ = TILE_SIZE / 360;
-  this.pixelsPerLonRadian_ = TILE_SIZE / (2 * Math.PI);
-}
-
-MercatorProjection.prototype.fromLatLngToPoint = function(latLng,
-    opt_point) {
-  var me = this;
-  var point = opt_point || new google.maps.Point(0, 0);
-  var origin = me.pixelOrigin_;
-
-  point.x = origin.x + latLng.lng() * me.pixelsPerLonDegree_;
-
-  // Truncating to 0.9999 effectively limits latitude to 89.189. This is
-  // about a third of a tile past the edge of the world tile.
-  var siny = bound(Math.sin(degreesToRadians(latLng.lat())), -0.9999,
-      0.9999);
-  point.y = origin.y + 0.5 * Math.log((1 + siny) / (1 - siny)) *
-      -me.pixelsPerLonRadian_;
-  return point;
-};
-
-MercatorProjection.prototype.fromPointToLatLng = function(point) {
-  var me = this;
-  var origin = me.pixelOrigin_;
-  var lng = (point.x - origin.x) / me.pixelsPerLonDegree_;
-  var latRadians = (point.y - origin.y) / -me.pixelsPerLonRadian_;
-  var lat = radiansToDegrees(2 * Math.atan(Math.exp(latRadians)) -
-      Math.PI / 2);
-  return new google.maps.LatLng(lat, lng);
-};
-
-function createInfoWindowContent() {
-  var numTiles = 1 << map.getZoom();
-  var projection = new MercatorProjection();
-  var worldCoordinate = projection.fromLatLngToPoint(language);
-  var pixelCoordinate = new google.maps.Point(
-      worldCoordinate.x * numTiles,
-      worldCoordinate.y * numTiles);
-  var tileCoordinate = new google.maps.Point(
-      Math.floor(pixelCoordinate.x / TILE_SIZE),
-      Math.floor(pixelCoordinate.y / TILE_SIZE));
-
-  return [
-    'Madrid, ES',
-    'LatLng: ' + language.lat() + ' , ' + language.lng(),
-    'World Coordinate: ' + worldCoordinate.x + ' , ' +
-      worldCoordinate.y,
-    'Pixel Coordinate: ' + Math.floor(pixelCoordinate.x) + ' , ' +
-      Math.floor(pixelCoordinate.y),
-    'Tile Coordinate: ' + tileCoordinate.x + ' , ' +
-      tileCoordinate.y + ' at Zoom Level: ' + map.getZoom()
-  ].join('<br>');
-}
-
-//Options to initialize the map
 function initialize() {
+  var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
   var mapOptions = {
-    zoom: 2,
+    zoom: 1,
+    minZoom:1,
     center: mapcenter
   };
 
-  //Paint map in div
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-  var coordInfoWindow = new google.maps.InfoWindow();
-  coordInfoWindow.setContent(createInfoWindowContent());
-  coordInfoWindow.setPosition(language);
-  coordInfoWindow.open(map);
+  for (var language in languageMap) {
+    var languageOptions = {
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      map: map,
+      center: languageMap[language].center,
+      radius: 10000
+    };
+    // Add the circle for this city to the map.
+    languageCircle = new google.maps.Circle(languageOptions);
 
-  google.maps.event.addListener(map, 'zoom_changed', function() {
-    coordInfoWindow.setContent(createInfoWindowContent());
-    coordInfoWindow.open(map);
+    //Text to display when hovering the languages in map
+	var contentString = '<div id="content">'+
+	    '<div id="siteNotice">'+
+	    '</div>'+
+	    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+	    '<div id="bodyContent">'+
+	    '<p><b>language 1</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+	    'language  bla bla</p>'+
+	    '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194"></p>'+
+	    'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+	    '</div>'+
+	    '</div>';
+
+	var infowindow = new google.maps.InfoWindow({
+	    content: contentString,
+	    position: languageMap[language].center,
+	    disableAutoPan:true,
+	    maxWidth: 150
+	});
+  }
+
+  google.maps.event.addListener(languageCircle, 'click', function() {
+  	infowindow.open(map);
   });
 }
 
-//Loading the map
 google.maps.event.addDomListener(window, 'load', initialize);
 
